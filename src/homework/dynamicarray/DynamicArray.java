7ebefaddf0fc6e549bf35742986c782eac69d319
JavaCore <smeri2455@gmail.com>
2025-10-08 00:24:23 +0400

    Տնային․Աշխատանք

--- src/homework/dynamicarray/DynamicArray.java
+++ src/homework/dynamicarray/DynamicArray.java
@@ -1,40 +1,92 @@
 package homework.dynamicarray;
 
 public class DynamicArray {
-        public static void main(String[] args) {
-
-        }
-        private int[] array = new int[10];
-
-        private int size = 0;
-        public void add(int value) {
-            if (size == array.length) {
-                extend();
-            }
-            array[size] = value;
-            size++;
-        }
-
-        private void extend() {
-            int[] newArray = new int[array.length + 10];
-            for (int i = 0; i < array.length; i++) {
-                newArray[i] = array[i];
-            }
-            array = newArray;
-        }
-
-        public int getByIndex(int index) {
-            if (index >= 0 && index < size) {
-                return array[index];
-            }
-            return -1;
-        }
-
-        public void print() {
-            for (int i = 0; i < size; i++) {
-                System.out.print(array[i] + " ");
-            }
-            System.out.println();
-        }
+    {
     }
 
+    private int[] array = new int[10];
+
+    private int size = 0;
+
+    public void add(int value) {
+        if (size == array.length) {
+            extend();
+        }
+        array[size] = value;
+        size++;
+    }
+
+    private void extend() {
+        int[] newArray = new int[array.length + 10];
+        for (int i = 0; i < array.length; i++) {
+            newArray[i] = array[i];
+        }
+        array = newArray;
+    }
+
+    public int getByIndex(int index) {
+        if (index >= 0 && index < size) {
+            return array[index];
+        }
+        return -1;
+    }
+
+    public void print() {
+        for (int i = 0; i < size; i++) {
+            System.out.print(array[i] + " ");
+        }
+        System.out.println();
+    }
+
+    public void deleteByIndex(int index) {
+        if (index < 0 || index >= size) {
+            System.out.println("No element with such index.");
+            return;
+        }
+        for (int i = index; i < size - 1; i++) {
+            array[i] = array[i + 1];
+        }
+        size--;
+    }
+
+    public void set(int index, int value) {
+        if (index < 0 || index >= size) {
+            System.out.println("No element with such index.");
+            return;
+        }
+        array[index] = value;
+    }
+
+    public void add(int index, int value) {
+        if (index < 0 || index > size) {
+            System.out.println("No such index.");
+            return;
+        }
+        if (size == array.length) {
+            extend();
+        }
+        for (int i = size; i > index; i--) {
+            array[i] = array[i - 1];
+        }
+        array[index] = value;
+        size++;
+    }
+
+    public boolean exists(int value) {
+        for (int i = 0; i < size; i++) {
+            if (array[i] == value) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public int getindexByValue(int value) {
+        for (int i = 0; i < size; i++) {
+            if (array[i] == value) {
+                return i;
+            }
+        }
+        return -1;
+    }
+}
