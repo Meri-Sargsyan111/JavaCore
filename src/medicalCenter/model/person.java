136eb209817e9880d120b2a86d0c8375fba05ffe
JavaCore <smeri2455@gmail.com>
2025-11-10 23:58:25 +0400

    simple test

---
+++ src/medicalCenter/model/person.java
@@ -0,0 +1,60 @@
+package medicalCenter.model;
+
+public class person {
+        private String id;
+        private String name;
+        private String surname;
+        private String phone;
+
+        public person(String id, String name, String surname, String phone) {
+            this.id = id;
+            this.name = name;
+            this.surname = surname;
+            this.phone = phone;
+        }
+
+        public person() {
+        }
+
+        public String getId() {
+            return id;
+        }
+
+        public void setId(String id) {
+            this.id = id;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public String getSurname() {
+            return surname;
+        }
+
+        public void setSurname(String surname) {
+            this.surname = surname;
+        }
+
+        public String getPhone() {
+            return phone;
+        }
+
+        public void setPhone(String phone) {
+            this.phone = phone;
+        }
+
+        @Override
+        public String toString() {
+            return "id='" + id + '\'' +
+                    ", name='" + name + '\'' +
+                    ", surname='" + surname + '\'' +
+                    ", phone='" + phone + '\'';
+        }
+    }
+
+
---
+++ src/medicalCenter/util/DateUtil.java
@@ -0,0 +1,17 @@
+package medicalCenter.util;
+
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class DateUtil {
+    private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm");
+
+    public static LocalDateTime parse(String dateStr) {
+        return LocalDateTime.parse(dateStr, FORMATTER);
+    }
+
+    public static String format(LocalDateTime dateTime) {
+        return dateTime.format(FORMATTER);
+    }
+}
